10581ec5bcbe5a29ea5a9919e8b05ccb9f59faa3
Jasper Seidel <sammy@jawsper.nl>
2013-01-14 12:13:20 +0100

    Stripped right whitepace off name for filenames. Made read_smth return False if nothing left to read. Added __init__ and get_class methods to class LocoFile. Read all chunks now. Added EOF check in read_chunk.

--- libloco/LocoFile.py
+++ libloco/LocoFile.py
@@ -8,22 +8,34 @@ class LocoFile:
 	pngbase = None
 	
 	def makefilename( self, ext ):
-		return os.path.relpath( 'j_{0}{1}'.format( self._name, ext ), os.path.dirname( self.file ) )
+		return os.path.relpath( 'j_{0}{1}'.format( self._name.rstrip(), ext ), os.path.dirname( self.file ) )
 	def makepngname( self, num ):
-		pngbase = self.pngbase if self.pngbase != None else 'j_{0}'.format( self._name )
+		pngbase = self.pngbase if self.pngbase != None else 'j_{0}'.format( self._name.rstrip() )
 		pngbase = os.path.relpath( pngbase, os.path.dirname( self.file ) )
 		if not os.path.isdir( pngbase ):
 			os.mkdir( pngbase )
-		return '{0}{1}{2:03}.png'.format( pngbase, os.sep, num )
+		return os.path.join( pngbase, '{0:03}.png'.format( num ) )
 
 	def read_smth( self, spec ):
-		return struct.unpack( spec, self.f.read( struct.calcsize( spec ) ) )[0]
+		r = self.f.read( struct.calcsize( spec ) )
+		if len( r ) == 0:
+			return False
+		return struct.unpack( spec, r )[0]
 	def read_int8( self ):
 		return self.read_smth( 'b' )
 	def read_uint8( self ):
 		return self.read_smth( 'B' )
 	def read_uint32( self ):
 		return self.read_smth( '<I' )
+		
+	def __init__( self, filename ):
+		self.filename = os.path.realpath( filename )
+		
+	def get_class( self ):
+		with open( self.filename, 'rb' ) as f:
+			_class = struct.unpack( 'B', f.read( 1 ) )[0]
+		return _class
+			
 		
 	def decode( self, filename ):
 		self.file = os.path.realpath( filename )
@@ -43,12 +55,14 @@ class LocoFile:
 			self._class &= 0x7F
 			
 			while self.read_chunk():
-				break
+				pass
 			self.xml.write( '</object>\n' )
 			self.xml.close()
 		
 	def read_chunk( self ):
 		compression = self.read_uint8()
+		if compression == False: # EOF
+			return False
 		length = self.read_uint32()
 		self.xml.write( '<chunk compression="{0}">\n'.format( compression ) )
 		if compression == 0:   # Raw
