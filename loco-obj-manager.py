4b0effc18fc6d4d9c1243831ebd8fbbd3294e097
Jasper Seidel <sammy@jawsper.nl>
2013-01-14 12:04:41 +0100

    Main file can now accept a directory.

--- loco-obj-manager.py
+++ loco-obj-manager.py
@@ -3,9 +3,19 @@
 from libloco import LocoFile
 
 import sys
-
+import os
 
 if __name__ == '__main__':
-	f = LocoFile()
-	f.decode( sys.argv[1] )
+	path = None
+	if len( sys.argv ) > 1:
+		if os.path.isdir( sys.argv[1] ):
+			path = os.path.realpath( sys.argv[1] )
+		else:
+			f = LocoFile( sys.argv[1] )
+			f.decode( sys.argv[1] )
+	else:
+		path = os.path.realpath( '.' )
+	if path != None:
+		for filename in os.listdir( path ):
+			print filename
 	pass
