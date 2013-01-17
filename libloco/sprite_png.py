a9006fa1d82bf31ca8755688837377013ea9e180
Jasper Seidel <desktop@jawsper.nl>
2013-01-17 01:28:06 +0100

    Implemented sprite encoding

--- libloco/sprite_png.py
+++ libloco/sprite_png.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import png
-from helper import uint8_t, getvalue
+from helper import uint8_t, getvalue, pack
 
 from locopal import *
 
@@ -51,7 +51,53 @@ def makepng( fname, data, width, height, flags ):
 	
 	png.close()
 	return dumped
+	
+def getspriterow( row, flags ):
+	if not flags & 4:
+		return row
+	x1 = x2 = 0
+	data = []
+	width = len( row )
+	offset = len( data )
+	while x1 < width:
+		while x1 < width and row[x1] == bgcol:
+			x1 += 1
+		if x1 < width:
+			length = 1
+			x2 = x1 + length
+			while x2 < width and length < 127 and row[x2] != bgcol:
+				x2 += 1
+				length += 1
+			offset = len( data )
+			data.append( length )
+			data.append( x1 )
+			for i in range( length ):
+				data.append( row[ x1 + i ] )
+		else: # transparent to end of line
+			if x2 == 0: # totally empty line
+				data.append( 0 )
+				data.append( 0 )
+			x2 = x1
+		x1 = x2
+	data[ offset ] |= 0x80
+	return data
 
+def readpng( fname, flags ):
+	r = png.Reader( filename = fname )
+	data = []
+	( width, height, pixels, metadata ) = r.read()
+	sprite_data = []
+	if flags & 4:
+		size = height * 2
+		y = 0
+	for row in pixels:
+		sprite_row = getspriterow( row, flags )
+		if flags & 4:
+			data.extend( pack( '<H', size + y ) )
+			y += len( sprite_row )
+		sprite_data.extend( sprite_row )
+	data.extend( sprite_data )
+	return ( data, width, height )
 
 class PNGWriter:
 	fp = None
