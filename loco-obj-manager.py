6f7c491053b147fedc4f5254e60d2889368da3e8
Jasper Seidel <desktop@jawsper.nl>
2013-01-17 04:03:09 +0100

    Implemented search function for file with a certain class in a folder. File will be copied to working dir. Warning: no overwrite test."

--- loco-obj-manager.py
+++ loco-obj-manager.py
@@ -7,8 +7,15 @@ import os
 
 if __name__ == '__main__':
 	path = None
+	test = None
 	if len( sys.argv ) > 1:
-		if os.path.isdir( sys.argv[1] ):
+		if sys.argv[1][0:2] == '-t':
+			test = int( sys.argv[1][2:], 16 if sys.argv[1][3] == 'x' else 10 )
+			if len( sys.argv ) > 2 and os.path.isdir( sys.argv[2] ):
+				path = os.path.realpath( sys.argv[2] )
+			else:
+				path = os.path.realpath( '.' )
+		elif os.path.isdir( sys.argv[1] ):
 			path = os.path.realpath( sys.argv[1] )
 		else:
 			if sys.argv[1].lower().endswith( '.xml' ):
@@ -23,5 +30,12 @@ if __name__ == '__main__':
 		for filename in os.listdir( path ):
 			if filename.lower().endswith( '.dat' ):
 				f = LocoDecoder( os.path.join( path, filename ) )
+				if test:
+					if f.get_header()[0] == test:
+						open( os.path.join( '.', filename ), 'wb' ).write( open( os.path.join( path, filename ), 'rb' ).read() )
+						print( '{2} class: 0x{0:02X} {3}'.format( *f.get_header() ) )
+						break
+					else:
+						continue
 				print( '{2} class: 0x{0:02X} {3}'.format( *f.get_header() ) )
 	pass
