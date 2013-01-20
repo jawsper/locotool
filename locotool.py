0db7da26249110177748a362debc4e7c251a9177
Jasper Seidel <vaio@jawsper.nl>
2013-01-20 15:47:22 +0100

    Changed the imports so they actually work...

--- locotool.py
+++ locotool.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-import libloco.LocoDecoder
-import libloco.LocoEncoder
+from libloco.LocoDecoder import LocoDecoder
+from libloco.LocoEncoder import LocoEncoder
 
 import sys
 
 if __name__ == '__main__':
 	if len( sys.argv ) > 1:
 		if sys.argv[1].lower().endswith( '.xml' ):
-			f = libloco.LocoEncoder( sys.argv[1] )
+			f = LocoEncoder( sys.argv[1] )
 			f.encode()
 		else:
-			f = libloco.LocoDecoder( sys.argv[1] )
+			f = LocoDecoder( sys.argv[1] )
 			f.decode()
