43d75fbdac6223cf76e80156c6292f91ddc74224
Jasper Seidel <sammy@jawsper.nl>
2013-01-21 20:39:03 +0100

    loco_obj_scanner: 	Added "find" argumentr to show all objects of certain class. 	Added "show-classes" argument to show all known object classes.

--- loco_obj_scanner.py
+++ loco_obj_scanner.py
@@ -3,6 +3,7 @@ from __future__ import print_function
 
 from libloco.LocoDecoder import LocoDecoder
 
+import sys
 import os
 import argparse
 
@@ -11,21 +12,35 @@ if __name__ == '__main__':
 	test = None
 	parser = argparse.ArgumentParser( description='Loco obj scanner' )
 	parser.add_argument( 'path', nargs = '?', default = '.' )
-	parser.add_argument( '-t', '--type', type=int )
-	parser.add_argument( '--all', action="store_true" )
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument( '--show-classes', action='store_true', help='Show known Locomotion object classes' )
+	group.add_argument( '-f', '--find', type=int, default=None, help='Only show objects of class FIND' )
+	group_type = group.add_argument_group()
+	group_type.add_argument( '-t', '--type', type=int, default=None, help='Copy the first object of class TYPE to the current dir' )
+	group_type.add_argument( '--all', action="store_true", help='In combination with type: copy all the objects of class TYPE' )
 	args = parser.parse_args()
+	
+	if args.show_classes:
+		from libloco.objects import objclassnames
+		cls = 0
+		for name in objclassnames:
+			print( 'Class {0:02} (0x{0:02X}): {1}'.format( cls, name ) )
+			cls += 1
+		sys.exit( 0 )
 	
 	if os.path.isdir( args.path ):
 		path = os.path.realpath( args.path )
 		for filename in sorted( os.listdir( path ), cmp=lambda x,y: cmp( x.lower(), y.lower() ) ):
 			if filename.lower().endswith( '.dat' ):
 				f = LocoDecoder( os.path.join( path, filename ) )
-				if args.type:
-					if f.get_header()[0] == args.type:
-						open( os.path.join( '.', filename ), 'wb' ).write( open( os.path.join( path, filename ), 'rb' ).read() )
+				if args.type != None or args.find != None:
+					cls = args.type or args.find
+					if f.get_header()[0] == cls:
 						print( '{2} class: {0:02} {3}'.format( *f.get_header() ) )
-						if not args.all:
-							break
+						if args.type != None:
+							open( os.path.join( '.', filename ), 'wb' ).write( open( os.path.join( path, filename ), 'rb' ).read() )
+							if not args.all:
+								break
 				else:
 					print( '{2} class: {0:02} {3}'.format( *f.get_header() ) )
 	else:
