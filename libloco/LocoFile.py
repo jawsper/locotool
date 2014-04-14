32c2edb68994f7756ec42cfd57d8212d20d694bf
Jasper Seidel <sammy@jawsper.nl>
2014-04-14 16:16:36 +0200

    Improve file location detection

--- libloco/LocoFile.py
+++ libloco/LocoFile.py
@@ -4,14 +4,24 @@ import os
 class LocoFile:
 	filename = None
 		
-	def __init__( self, filename ):
+	def __init__( self, filename, **kwargs ):
 		self.filename = os.path.realpath( filename )
+		self.output_dir = kwargs['output_dir'] if 'output_dir' in kwargs else '.'
+		#print self.filename
 	
+	def get_basedir(self):
+		return os.path.realpath('.')
 	def makefilename( self, ext ):
-		return os.path.relpath( 'j_{0}{1}'.format( self._name.rstrip(), ext ), os.path.dirname( self.filename ) )
+		filename = 'j_{}{}'.format( self._name.rstrip(), ext )
+		return os.path.join(self.output_dir, filename) if self.output_dir != '.' else filename
+		#return os.path.relpath( 'j_{0}{1}'.format( self._name.rstrip(), ext ), os.path.dirname( self.filename ) )
 	def makepngname( self, num ):
-		pngbase = self.pngbase if self.pngbase != None else 'j_{0}'.format( self._name.rstrip() )
-		pngbase = os.path.relpath( pngbase, os.path.dirname( self.filename ) )
+		#pngbase = self.pngbase if self.pngbase != None else 'j_{0}'.format( self._name.rstrip() )
+		#print pngbase
+		#pngbase = os.path.relpath( pngbase, os.path.dirname( self.filename ) )
+		#print pngbase
+		pngbase = self.makefilename('')
+		#print pngbase
 		if not os.path.isdir( pngbase ):
 			os.mkdir( pngbase )
 		return os.path.join( pngbase, '{0:03}.png'.format( num ) )
